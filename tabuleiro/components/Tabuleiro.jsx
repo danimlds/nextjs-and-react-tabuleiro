f2fbe73f856a71de8f1fd18f16aab817cb92c82b
Daniel Moura <daniel.mlds@gmail.com>
2022-06-01 20:52:23 -0300

    Projeto Tabuleiro com Nextjs e React

---
+++ tabuleiro/components/Tabuleiro.jsx
@@ -0,0 +1,19 @@
+import Linha from "./Linha";
+
+export default function Tabuleiro() {
+    return(
+        <div>
+            <Linha />
+            <Linha preta />
+            
+            <Linha />
+            <Linha preta />
+            
+            <Linha />
+            <Linha preta />
+            
+            <Linha />
+            <Linha preta />
+        </div>
+    )
+}
